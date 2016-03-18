199ebfc750f53c76bef88be77ee706f45c87167a
mirian <roxsis1998@gmail.com>
2016-03-18 00:47:21 -0500

    trabajo final

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">PUNO-AZÁNGARO</a>
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">AZÁNGARO</a></li>
+					<li><a href="#">AZÁNGARO</a></li>
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">AZÁNGARO</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+	<nav>
+	<h1>BIENVENIDO A MI PAGINA WEB </h1>
+		<ul class="menu-lista">
+			<li class="menu-item"><a href="INICIO.html" class="menu-link">INICIO</a></li>
+			<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+			<li class="menu-item"><a href="" class="menu-link">GANADERIA</a></li>
+			<li class="menu-item"><a href="" class="menu-link">AGRICULTURA</a></li>
+			<li class="menu-item"><a href="" class="menu-link">MINERIA</a></li>
+		</ul>
+	</nav>
+	<br>
+	<div id="carousel-id" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="item">
+				<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/img/1.jpg" height="500" width="1200">
+					
+				</header><!-- /header -->>
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Example headline.</h1>
+						<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/img/2.jpg" height="500" width="1200">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Another example headline.</h1>
+						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item active">
+				<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="public/img/3.jpg" height="500" width="1200">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>One more for good measure.</h1>
+						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	</div>
+ <br>
+	<script src="public/js/jquery-1.12.1.js"></script>
+	<script src="public/js/bootstrap.js"></script>
+	<h1>HISTORIA DE PUNO</h1>
+	<h2>Entre los años 800 y 1200 d.C. el altiplano, compartido hoy por Perú y Bolivia, fue testigo del desarrollo de la cultura Tiahuanaco. Una civilización cuya influencia, especialmente en el ámbito religioso, se dejo sentir en casi todo el territorio peruano. Con destreza desarrollaron la arquitectura, la litoescultura, la cerámica y la textilería, dejando en todas sus piezas, una iconografía religiosa que aún no comprendemos a cabalidad. Al decaer el poder Tiahuanaco, diversas culturales locales surgieron en sus antiguos dominios. Los Collas y Aymaras destacaron sobre los demás reinos.
+
+Los Incas, en el siglo XV, entraron en la meseta del Collao conquistando violentamente a los grupos collas que habitaban tanto la parte alta como las tierras bajas. Otros pueblos como los lupacas, pacajes y azángaros optaron más bien por aliarse a los Incas y establecieron vínculos de reciprocidad con los gobernantes cusqueños.
+
+Los conquistadores españoles establecidos en Cusco tuvieron noticias de las riquezas que guardaba la región del Collao. Empezó así, la presencia hispana en el Altiplano.
+
+A mediados del siglo XVII, el control de las minas de Laycacota ocasionó terribles disputas. El propio virrey Conde de Lemos viajó hasta la zona para pacificarla y fundó entonces la actual ciudad de Puno con el nombre de San Carlos de Puno el 4 de noviembre de 1668. Más tarde, durante el siglo XVIII, la población indígena de la región apoyó la rebelión de Túpac Amaru y la de Túpac Catari, y exigió poner fin a los abusos cometidos por las malas autoridades.
+
+En el siglo XX los campesinos puneños se levantaron nuevamente, esta vez con Teodomiro Gutiérrez Cuevas Rumi Maqui como líder, lucharon contra aquellos que pretendían arrebatarles sus tierras.
+
+La tradición campesina se ha mantenido en el departamento cuyas principales actividades continúan siendo la agricultura y la ganadería con miras al mercado regional del sur andino.</h2>
+	<video src="public/video/PUNO, PERU (video promocional).mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+	<video src="public/video/LAGO TITICACA - DOCUMENTAL 'Maravilla Natural del Mundo'.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+	<img src="public/img/2.jpg" alt="500" width="1300">
+	<h1>COSTUMBRE DE PUNEÑOS</h1>
+	<img src="public/img/4.jpg" height="350" width="302">
+    <img src="public/img/5.jpg" height="350" width="302">
+    <img src="public/img/6.jpg" height="350" width="302">
+    <img src="public/img/7.jpg" height="350" width="302">
+    <h1>IMAGENES DE AZANGARO-PUNO</h1>
+    <div class="container">
+		<div class="row">
+			<div class="col-md-4"><img src="public/img/mies.jpg" class="img-"alt=""></div>
+			<div class="col-md-4"><img src="public/img/mie.jpg" class="img-circle" alt=""></div>
+	
+			<div class="col-md-4"><img src="public/img/mi.jpg" class="img-responsive" alt="">
+			</div>
+			</div>
+			<h1>COSTUMBRES DE PUNO-PERÚ</h1>
+<br>
+	
+</div>
+<div class="col-md-4"
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">PLATOS TIPICOS PUNO-PERÚ</h3>
+       
+	</div> 
+
+	<div class="panel-body">
+     
+	<img src="public/img/PLA.jpg" alt="">
+
+	<p>La comida puneña se caracteriza por el calor de un horno de leña, usar sus ollas dev barro y todo el aroma que se relaciona con la madre tierra. La mayoría de sus ingredientes son carnes, tubérculos, hierbas, granos que son utilizados en gran parte en sus platos típicos.
+Muchos de sus platillos son originarios del lugar y otros han sido influenciados por sus vecinos bolivianos. Estos ricos potajes los pueden encontrar en los restaurantes, picanterías, ferias, las grandes fiestas patronales y en muchas casas de hospedaje. Se sirven acompañados de chicha morada o de una auténtica chicha de jora y otros con un pisco acholado.
+1.  Chupe de quinua
+Es un plato muy alimenticio. Es a base de quinua, se cocina en agua fría sin sal. Se le agrega verduras.
+2.  Trucha frita
+Es un pescado que se adquiere del Lago Titicaca que contiene proteínas, así como vitaminas y minerales.
+3.  Caldo de cabeza o Huarjata
+Para preparar este plato se debe poner a cocer una cabeza de chancho (cerdo) o de cordero en trozos
+4.  Chairo o Chayro
+Es una sopa espesa y sabrosa a base de papas, chuño, carne y otros ingredientes. Muy apetitoso y de alto poder nutritivo.
+5.  Chicharrón de alpaca
+Es un delicioso y nutritivo potaje de Puno. Primero se cuece la carne hasta que se consume el agua, luego con la misma grasa se dora.</p>
+	
+		
+	</div>
+</div>
+<div class="col-md-4"
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">GANADERIA PUNO-PERU</h3>
+	</div>
+	<div class="panel-body">
+	<img src="public/img/ga.jpg" alt="">
+	<p>Los inicios de la Empresa Ganadera “El Rosario” se remonta a 1917, cuando el Sr. Jose T. Bellido compra el Fundo Queque Norte y empiezan una larga trayectoria de proyectos de desarrollo en ganadería, agricultura e industria láctea.
+
+Es a partir de los años 50 que el Dr. Jorge Bellido Núñez, gran profesional en Medicina Veterinaria, se hace cargo del Fundo y le da un gran impulso de modernidad en ganadería con mejoramiento genético, manejo de pasturas y forrajes e innovación en la Industria Láctea.
+
+El Dr. Bellido Núñez fue pionero en la inseminación artificial en ovinos y uno de los primeros en lograr la transformación de la industria láctea del departamento de Puno.
+
+Actualmente su hijo el Sr. Jorge Bellido Lopera, que estuvo al lado de su padre desde finales de la década del 70, continúa con el proceso de transformación y modernización del Fundo, logrando tener uno de los Establos líderes en la sierra Peruana y un Hato de Ganado Brown Swiss Registrado de alta producción y adaptadas a más de 4000 m.s.n.m.
+
+Es así que luego de varias décadas se logra cerrar un círculo de producción y productividad que va desde la obtención de leche de buena calidad hasta su transformación en productos lácteos logrados con altos estándares de manufactura, distribuidos en los mercados más exigentes del país.
+
+</p>
+		
+	</div>
+	</div>
+<div class="col-md-4"
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">DANZA DE PUNO-PERÚ</h3>
+	</div>
+	<div class="panel-body">
+	<img src="public/img/images.jpg" alt="">
+	<p>	
+HISTORIA DE LA KULLAWADA 
+por: Edwin Mamani Huacani 
+
+Kullawada danza de los hilanderos:
+
+La danza de los “Kullawas” conocida también como “Kullawada” proviene del tiempo de incario y está vinculada con la actividad de hilanderos y tejedores antiguos Algunos Historiadores sostiene que esta danza perteneció a la nobleza incaica, era bailada por las ñustas y autoridades del imperio. Otros investigadores sostienen que varias parejas aymaras llevados a la capital del imperio danzaban ceremonialmente para el deleite del Inca y los orejones (nobles cuzqueños) dirigidos por un guía, portando una gran rueca con copos de lana, para representar el oficio a la que se dedicaba su etnia, y la importancia que tenía en la economía, cultura y comunicación Aymara.
+
+Según Sanginés, “La kullawada, danza ágil, y plástica, con una coreografía de pasos dobles, con pocas figuras y giros intercalados. Tiene una vinculación directa a la actividad textil, de gran importancia en las relaciones sociales y de reciprocidad entre los pueblos. Es una de las danzas más hermosas bailada por los criadores de los camélidos Sudaméricanos domesticados (llamas y alpacas)…”
+
+</p>
+		
+	<div>
+		<audio autoplay="trve">
+			<source src="public/audio/Huajcha Puquito - huayno.mp3" >
+
+</body>
+</html>
